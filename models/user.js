0f3d0abf869d50b0ad0f0e240dc66fa4fe86afcd
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-05 00:35:52 +0300

    feat: user schema create

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    age: { type: Number, min: 18, max: 60 },
+    role: {
+        type: String,
+        enum: {
+            values: ["admin", "viewer", "editor", "user"],
+            message: "{VALUE} is not supported",
+        },
+        default: "user",
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
