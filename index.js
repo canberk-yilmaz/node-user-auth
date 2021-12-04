9b89ffeb0e43b4b4d219da92a8a95551e786365f
canberk-yilmaz <cyilmaz089@gmail.com>
2021-12-05 00:35:40 +0300

    feat: user route add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const user = require('./router/user')
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use('/user', user)
+
 
 mongoose.connect(
     process.env.DATABASE,
